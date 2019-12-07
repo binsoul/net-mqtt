3e112ef5741abe0b8bb49c43c09ba6bb1566eadb
Sebastian Mößler <s.moeszler@binsoul.de>
2019-12-07 01:01:07 +0100

    Remove unused imports

--- src/PacketFactory.php
+++ src/PacketFactory.php
@@ -3,20 +3,6 @@
 namespace BinSoul\Net\Mqtt;
 
 use BinSoul\Net\Mqtt\Exception\UnknownPacketTypeException;
-use BinSoul\Net\Mqtt\Packet\ConnectRequestPacket;
-use BinSoul\Net\Mqtt\Packet\ConnectResponsePacket;
-use BinSoul\Net\Mqtt\Packet\DisconnectRequestPacket;
-use BinSoul\Net\Mqtt\Packet\PingRequestPacket;
-use BinSoul\Net\Mqtt\Packet\PingResponsePacket;
-use BinSoul\Net\Mqtt\Packet\PublishRequestPacket;
-use BinSoul\Net\Mqtt\Packet\PublishAckPacket;
-use BinSoul\Net\Mqtt\Packet\PublishCompletePacket;
-use BinSoul\Net\Mqtt\Packet\PublishReceivedPacket;
-use BinSoul\Net\Mqtt\Packet\PublishReleasePacket;
-use BinSoul\Net\Mqtt\Packet\SubscribeRequestPacket;
-use BinSoul\Net\Mqtt\Packet\SubscribeResponsePacket;
-use BinSoul\Net\Mqtt\Packet\UnsubscribeRequestPacket;
-use BinSoul\Net\Mqtt\Packet\UnsubscribeResponsePacket;
 
 /**
  * Builds instances of the {@see Packet} interface.
