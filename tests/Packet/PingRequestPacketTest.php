c10a4025a3b488755a7e1e2632d5684353329eb9
Sebastian Mößler <s.moeszler@binsoul.de>
2019-12-08 13:01:51 +0100

    Test the ping request and response packets

---
+++ tests/Packet/PingRequestPacketTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BinSoul\Test\Net\Mqtt\Packet;
+
+use BinSoul\Net\Mqtt\Packet;
+use BinSoul\Net\Mqtt\Packet\PingRequestPacket;
+use BinSoul\Net\Mqtt\PacketStream;
+use PHPUnit\Framework\TestCase;
+
+class PingRequestPacketTest extends TestCase
+{
+    private function getDefaultData(): string
+    {
+        return "\xc0\x00";
+    }
+
+    public function test_write(): void
+    {
+        $packet = new PingRequestPacket();
+        $stream = new PacketStream();
+        $packet->write($stream);
+
+        $this->assertEquals($this->getDefaultData(), $stream->getData());
+    }
+
+    public function test_read(): void
+    {
+        $stream = new PacketStream($this->getDefaultData());
+        $packet = new PingRequestPacket();
+        $packet->read($stream);
+
+        $this->assertEquals(Packet::TYPE_PINGREQ, $packet->getPacketType());
+    }
+}
---
+++ tests/Packet/PingResponsePacketTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BinSoul\Test\Net\Mqtt\Packet;
+
+use BinSoul\Net\Mqtt\Packet;
+use BinSoul\Net\Mqtt\Packet\PingResponsePacket;
+use BinSoul\Net\Mqtt\PacketStream;
+use PHPUnit\Framework\TestCase;
+
+class PingResponsePacketTest extends TestCase
+{
+    private function getDefaultData(): string
+    {
+        return "\xd0\x00";
+    }
+
+    public function test_write(): void
+    {
+        $packet = new PingResponsePacket();
+        $stream = new PacketStream();
+        $packet->write($stream);
+
+        $this->assertEquals($this->getDefaultData(), $stream->getData());
+    }
+
+    public function test_read(): void
+    {
+        $stream = new PacketStream($this->getDefaultData());
+        $packet = new PingResponsePacket();
+        $packet->read($stream);
+
+        $this->assertEquals(Packet::TYPE_PINGRESP, $packet->getPacketType());
+    }
+}
